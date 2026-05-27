5ae12673a96e2e9cb39ca46f749934dbe534e005
MoyeAnthonia <moye.anthonia@gmail.com>
2026-05-27 12:17:52 +1000

    task 5 done

--- TypeScript/task_5/js/main.ts
+++ TypeScript/task_5/js/main.ts
@@ -1,75 +1,23 @@
-// 1. Interfaces
-interface DirectorInterface {
-  workFromHome(): string;
-  getCoffeeBreak(): string;
-  workDirectorTasks(): string;
+interface MajorCredits {
+  credits: number;
+  __brand: 'MajorCredits';
 }
 
-interface TeacherInterface {
-  workFromHome(): string;
-  getCoffeeBreak(): string;
-  workTeacherTasks(): string;
+interface MinorCredits {
+  credits: number;
+  __brand: 'MinorCredits';
 }
 
-// 2. Director Class
-class Director implements DirectorInterface {
-  workFromHome(): string {
-    return "Working from home";
-  }
-
-  getCoffeeBreak(): string {
-    return "Getting a coffee break";
-  }
-
-  workDirectorTasks(): string {
-    return "Getting to director tasks";
-  }
+function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
+  return {
+    credits: subject1.credits + subject2.credits,
+    __brand: 'MajorCredits'
+  };
 }
 
-// 3. Teacher Class
-class Teacher implements TeacherInterface {
-  workFromHome(): string {
-    return "Cannot work from home";
-  }
-
-  getCoffeeBreak(): string {
-    return "Cannot have a break";
-  }
-
-  workTeacherTasks(): string {
-    return "Getting to work";
-  }
-}
-
-// 4. Factory Function
-function createEmployee(salary: number | string): Director | Teacher {
-  if (typeof salary === "number" && salary < 500) {
-    return new Teacher();
-  }
-  return new Director();
-}
-
-// 1. Type Predicate Function
-function isDirector(employee: Director | Teacher): employee is Director {
-  return (employee as Director).workDirectorTasks !== undefined;
-}
-
-// 2. Execution Function
-function executeWork(employee: Director | Teacher): void {
-  if (isDirector(employee)) {
-    console.log(employee.workDirectorTasks());
-  } else {
-    console.log(employee.workTeacherTasks());
-  }
-}
-
-// 1. String Literal Type
-type Subjects = "Math" | "History";
-
-// 2. Function Implementation
-function teachClass(todayClass: Subjects): string {
-  if (todayClass === "Math") {
-    return "Teaching Math";
-  }
-  return "Teaching History";
+function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
+  return {
+    credits: subject1.credits + subject2.credits,
+    __brand: 'MinorCredits'
+  };
 }
